19021aa837b8a242ee7f87a98fd1c256e279f759
Jonathan Potter <jonathan.d.potter@outlook.com>
2022-11-12 12:24:45 -0500

    Updated about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,22 @@
   <div
     class="d-flex h-75 p-5 flex-column align-items-center justify-content-around"
   >
-    <h3>Version 1.0.0</h3>
-    <router-link to="/">Go Home</router-link>
+    <h3>About Task Tracker</h3>
+    <p class="indented">
+      This is a modification of the Traversey Media
+      <a href="https://youtu.be/qZXt1Aom3Cs">Vue Crash Course Task Tracker</a>
+      app. I added Vuex state management and Bootstrap styling and a full back
+      end with a database to the basic tutorial project The back end is hosted
+      on heroku
+      <a href="https://jonathan-potter-task-tracker.herokuapp.com/">here</a>.
+    </p>
+    <p class="indented">
+      You can add tasks, double-click them to toggle reminders (the green
+      highlight) and delete tasks. There is no login requirement, so everyone
+      has the same list of tasks. I may add a login in the future.
+    </p>
+    <span class="fs-3">
+      <router-link to="/">Go Home</router-link>
+    </span>
   </div>
 </template>
